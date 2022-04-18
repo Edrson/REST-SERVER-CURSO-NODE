3fefe09743145bde63c26f4ce933e83c45f4044f
Ederson <ederson.ramirez87@gmail.com>
2022-04-18 02:34:04 -0600

    Update4

--- database/config.js
+++ database/config.js
@@ -4,6 +4,8 @@ require('colors');
 
 const dbConnection = async () => {
     try {
+        console.log('CNN>', process.env.MONGODB_CNN);
+
         await mongoose.connect(process.env.MONGODB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true
